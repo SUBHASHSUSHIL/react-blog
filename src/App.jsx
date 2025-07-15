3059979df4183c67fd005dbc79507a60648d2bbd
SUSHIL THAKUR <mt3028788@gmail.com>
2025-07-15 23:03:39 +0530

    importing and exporting components in React.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Header from "./Header"
+import Login, { Dashboard, Profile, Settings } from "./UserComponent";    // Importing the Header component and the Login component as default,
 
 function App() {  // This is the main App component
-  // The App component renders the Header and some content
-  // It also includes an Apple component and a sum function
   
-  alert(sum())  // This will alert the sum of two numbers when the App component is rendered
+  //alert(sum())  // This will alert the sum of two numbers when the App component is rendered
   return (
     <div>
       <Header />
-      <h1>Welcome to My App</h1>
-      <p>This is a simple React application.</p>
       <Apple />
+      <Login />
+      <Profile />
+      <Settings />
+      <Dashboard />
     </div>
   );
 }
---
+++ src/UserComponent.jsx
@@ -0,0 +1,33 @@
+function Login(){
+    return(
+        <div>
+            <h1>Login Component</h1>
+        </div>
+    )
+}
+
+export default Login;       // This is the default export for the UserComponent file single time inject 
+
+export function Profile(){       // This is a named export for the Profile component
+    return(
+        <div>
+            <h1>Profile Component</h1>
+        </div>
+    )
+}
+
+export function Settings(){
+    return(
+        <div>
+            <h1>Settings Component</h1>
+        </div>
+    )
+}
+
+export function Dashboard(){        // This is a named export for the Dashboard component
+    return(
+        <div>
+            <h1>Dashboard Component</h1>
+        </div>
+    )
+}
