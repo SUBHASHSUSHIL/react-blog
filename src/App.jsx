facc737355eed46b91399a2bece61640572a97bd
SUSHIL THAKUR <mt3028788@gmail.com>
2025-07-21 13:59:19 +0530

    clock add system time

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import LoopinArrayMapFunction from "./LoopinArrayMapFunction";
+import { useState } from "react";
+import Clock from "./Clock";
 
 
 function App() {  
-    
+    const [color, setColor] = useState('red');
   return (
     <div>
-      <LoopinArrayMapFunction />  
+     <h1>Digital Clock in React js</h1>
+     <select onChange={(event) => setColor(event.target.value)}>
+      <option value="yellow">Yellow</option>
+      <option value="red">Red</option>
+      <option value="orange">Orange</option>
+      <option value="green">Green</option>
+     </select>
+     <Clock color={color} />
     </div>
   );
 }
