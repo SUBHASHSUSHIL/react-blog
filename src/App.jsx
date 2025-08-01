2fa6f61bdbb826df3aa518e5458160c95dccba88
SUSHIL THAKUR <mt3028788@gmail.com>
2025-08-01 11:16:29 +0530

    use in inline InlineStyleCSS component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import { useState } from "react";
-import Counter from "./Counter";
-
-
-
+import InlineStyleCSS from "./InlineStyleCSS";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState(0);
-  const [display, setDisplay] = useState(true);
+  
 
   return (
     <div>
-    {
-      display ?<Counter count={count} data={data}/>:null     /* example in unmounting */
-    }
-    <button onClick={()=>setCount(count + 1)} >Counter</button>
-    <button onClick={()=>setData(data + 1)}>Data</button> 
-     <button onClick={()=>setDisplay(!display)}>toggle</button>  {/* example in unmounting */}
+    <InlineStyleCSS />
     </div>
   );
 }
--- src/InlineStyleCSS.jsx
+++ src/InlineStyleCSS.jsx
@@ -1,7 +1,97 @@
-function InlineStyleCSS(){
-    return(
+function InlineStyleCSS() {
+    const cardStyle = {
+        border: "1px solid #cccccc3b",
+        width: "200px",
+        boxShadow: "1px 2px 3px 0px #cccccc57",
+        margin: "10px",
+        padding: "10px"
+    }
+
+    return (
         <>
-        <div style={{color:'red', backgroundColor:'green'}}>Hello World</div>
+            <div style={{ color: 'red', backgroundColor: 'white', fontSize: '50px', borderRadius: '10px' }}>Hello World</div>
+            <div style={{ display: "flex" }}>
+                <div style={{ cardStyle }}>
+                    <img style={{ width: '200px' }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" srcSet="" />
+                    <div style={{ padding: '5px' }}>
+                        <h4>Sushil Thakur</h4>
+                        <p>Software Developer</p>
+                    </div>
+                </div>
+                <div style={{ cardStyle }} >
+                    <img style={{ width: '200px' }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" srcSet="" />
+                    <div style={{ padding: '5px' }}>
+                        <h4>Sushil Thakur</h4>
+                        <p>Software Developer</p>
+                    </div>
+                </div>
+                <div style={{ display: "flex flexWrap" }}>
+                    <div style={{ cardStyle }}>
+                        <img style={{ width: '200px' }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" srcSet="" />
+                        <div style={{ padding: '5px' }}>
+                            <h4>Sushil Thakur</h4>
+                            <p>Software Developer</p>
+                        </div>
+                    </div>
+                    <div style={{ cardStyle }} >
+                        <img style={{ width: '200px' }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" srcSet="" />
+                        <div style={{ padding: '5px' }}>
+                            <h4>Sushil Thakur</h4>
+                            <p>Software Developer</p>
+                        </div>
+                    </div>
+                    <div style={{ display: "flex flexWrap" }}>
+                        <div style={{ cardStyle }}>
+                            <img style={{ width: '200px' }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" srcSet="" />
+                            <div style={{ padding: '5px' }}>
+                                <h4>Sushil Thakur</h4>
+                                <p>Software Developer</p>
+                            </div>
+                        </div>
+                        <div style={{ cardStyle }} >
+                            <img style={{ width: '200px' }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" srcSet="" />
+                            <div style={{ padding: '5px' }}>
+                                <h4>Sushil Thakur</h4>
+                                <p>Software Developer</p>
+                            </div>
+                        </div>
+                        <div style={{ display: "flex flexWrap" }}>
+                            <div style={{ cardStyle }}>
+                                <img style={{ width: '200px' }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" srcSet="" />
+                                <div style={{ padding: '5px' }}>
+                                    <h4>Sushil Thakur</h4>
+                                    <p>Software Developer</p>
+                                </div>
+                            </div>
+                            <div style={{ cardStyle }} >
+                                <img style={{ width: '200px' }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" srcSet="" />
+                                <div style={{ padding: '5px' }}>
+                                    <h4>Sushil Thakur</h4>
+                                    <p>Software Developer</p>
+                                </div>
+                            </div>
+                            <div style={{ display: "flex flexWrap" }}>
+                                <div style={{ cardStyle }}>
+                                    <img style={{ width: '200px' }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" srcSet="" />
+                                    <div style={{ padding: '5px' }}>
+                                        <h4>Sushil Thakur</h4>
+                                        <p>Software Developer</p>
+                                    </div>
+                                </div>
+                                <div style={{ cardStyle }} >
+                                    <img style={{ width: '200px' }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" srcSet="" />
+                                    <div style={{ padding: '5px' }}>
+                                        <h4>Sushil Thakur</h4>
+                                        <p>Software Developer</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
+
+export default InlineStyleCSS;
